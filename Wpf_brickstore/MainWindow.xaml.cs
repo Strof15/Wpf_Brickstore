c1ffbfebedf8ced9869514c8d4b29a0fd70f8bbf
Kristof <robertnagykristof@gmail.com>
2024-09-06 12:28:59 +0200

    Letesztelve és némi hiba kijavitva!

--- Wpf_brickstore/MainWindow.xaml.cs
+++ Wpf_brickstore/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Wpf_brickstore
 {
